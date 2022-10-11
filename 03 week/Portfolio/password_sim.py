2a29266d8b51df46fdcfbdce4e06cafa719c5b31
JDennis21 <114511121+JDennis21@users.noreply.github.com>
2022-10-11 15:12:48 +0100

    Simulates setting a password

--- 03 week/Portfolio/password_sim.py
+++ 03 week/Portfolio/password_sim.py
@@ -1,8 +1,8 @@
 while True:
     pass1 = input('Choose a password to secure your account.\n')
     pass2 = input('Verify password.\n')
-    if pass1 == pass2:
+    if pass1 != pass2:
+        print('Passwords do not match, Try again')
+    else:
         print('Password has been set.')
         break
-    else:
-        print('Passwords do not match, Try again')
