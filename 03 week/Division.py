92eeb4c169ad3e654264f8a738e85fa849864a76
JDennis21 <114511121+JDennis21@users.noreply.github.com>
2022-10-11 14:04:48 +0100

    Divides an integer by another

---
+++ 03 week/Division.py
@@ -0,0 +1,6 @@
+x = int(input('Insert an integer.\n'))
+y = int(input('Insert another integer.\n'))
+if x == 0 or y == 0:
+    print('Division of or by zero is not possible.')
+else:
+    print('The result of', x, 'divided by', y, 'equals', x / y)
