f3762bec8e60e70225a034ca156206824b8542c5
JDennis21 <114511121+JDennis21@users.noreply.github.com>
2022-10-12 14:01:40 +0100

    Judges password length

--- 03 week/Portfolio/password_sim2.0.py
+++ 03 week/Portfolio/password_sim2.0.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python 3
 
 if __name__ == '__main__':
+    pass1 = input('Choose a password to secure your account\n*')
     while True:
-        pass1 = input('Choose a password to secure your account.\n')
-        pass2 = input('Verify password.\n')
-        if pass1 != pass2:
-            print('Passwords do not match, Try again')
+        if 8 <= len(pass1) <= 12:
+            pass2 = input('Verify password\n*')
+            if pass1 == pass2:
+                print('Password has been set.')
+                break
+            else:
+                pass1 = input('Passwords do not match try again\n*')
         else:
-            print('Password has been set.')
-            break
+            pass1 = input('Password must be between 8-12 characters\n*')
