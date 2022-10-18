0e104b244fc6bd1f4634847f3695d5803cd76a36
JDennis21 <114511121+JDennis21@users.noreply.github.com>
2022-10-18 20:11:00 +0100

    changed while loop to for loop

--- Portfolio/password_generator.py
+++ Portfolio/password_generator.py
@@ -1,27 +1,24 @@
-#!/usr/bin/env python 3
+#!/usr/bin/env python3
+
+from random import choice
 
 if __name__ == '__main__':
-    import random
 
     LIST_OF_WORDS = ['dog', 'ham', 'cat', 'knight', 'mighty', 'beast', 'bat', 'cheese', 'knuckles', 'mouse', 'black',
                      'white', 'red', 'purple', 'green', 'big', 'small', 'massive', 'tiny', 'round', 'square', 'minus',
-                     'plus', 'course']
+                     'plus', 'course', 'phone', 'home', 'feeling', 'happy', 'sad', 'safe', 'careless', 'unhappy']
 
-    print('Password Generator\n===================')
+    print('Password Generator\n==================')
     num_students = input('How many passwords are required?\n')
-    print('===================')
-
-    count = 1
 
     while True:
         try:
             if 1 > int(num_students) or int(num_students) > 24:
-                num_students = input('Please enter a number between 1 and 24.\n==================\n')
+                num_students = input('Please enter a number between 1 and 24.\n')
             else:
-                password = random.choice(LIST_OF_WORDS) + random.choice(LIST_OF_WORDS) + random.choice(LIST_OF_WORDS)
-                print(f'{count}-->{password}')
-                count += 1
-                if count - 1 == int(num_students):
-                    break
+                for count in range(int(num_students)):
+                    password = choice(LIST_OF_WORDS) + choice(LIST_OF_WORDS) + choice(LIST_OF_WORDS)
+                    print(f'{count + 1}-->{password}')
+                break
         except ValueError:
             num_students = input('Error: Please enter an integer.\n')
