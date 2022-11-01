da286c49826041729a80694720dc06afdc39243d
JDennis21 <114511121+JDennis21@users.noreply.github.com>
2022-11-01 15:29:44 +0000

    Grammatical error fixed

--- mid_point_assessment/cafe_menu.py
+++ mid_point_assessment/cafe_menu.py
@@ -2,4 +2,4 @@
 
 if __name__ == '__main__':
     spam_amount = int(input('How many slices of spam?\n'))
-    print(f'Egg with spam{",spam" * (spam_amount - 2)} {"and spam " if spam_amount > 1 else ""}coming up!')
+    print(f'Egg with spam{", spam" * (spam_amount - 2)} {"and spam " if spam_amount > 1 else ""}coming up!')
