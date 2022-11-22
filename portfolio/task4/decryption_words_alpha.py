e2b0286f9c111830f1e74592ac07c626d698d74f
JDennis21 <114511121+JDennis21@users.noreply.github.com>
2022-11-22 20:13:10 +0000

    fixed

--- portfolio/task4/decryption_words_alpha.py
+++ portfolio/task4/decryption_words_alpha.py
@@ -32,18 +32,18 @@ if __name__ == '__main__':
                 encrypted_str += ''.join(line)
                 no_spec_encrypted += ''.join(char for char in line if char.isalpha() or char == ' ')
 
-        for num in range(25):
+        for num in range(26):
             shifted_str_list = shift_string(no_spec_encrypted.lower(), num).split()
             correct_word = 0
             for word in shifted_str_list:
                 if word in eng_dict[word[0]] and num > 0:
                     correct_word += 1
-                    if correct_word / len(shifted_str_list) >= 0.70:
-                        print(f'\n{shift_string(encrypted_str, num)}')
-                        break
-                    elif num == 25 and correct_word / len(shifted_str_list) < 0.70:
-                        print('Cannot decrypt. Most likely not a caesar cipher at work here.')
-                        break
+            if correct_word / len(shifted_str_list) >= 0.70:
+                print(f'\n{shift_string(encrypted_str, num)}')
+                break
+            elif num == 25 and correct_word / len(shifted_str_list) < 0.70:
+                print('Cannot decrypt. Most likely not a caesar cipher at work here.')
+                break
 
     except FileNotFoundError:
         print('Error: File does not exist.')
