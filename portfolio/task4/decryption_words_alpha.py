69fb29ca52f1cd1ce8f18686ee006b9f28170f90
JDennis21 <114511121+JDennis21@users.noreply.github.com>
2022-12-06 21:23:02 +0000

    Changed to work with dictionary sorted to first two characters

--- portfolio/task4/decryption_words_alpha.py
+++ portfolio/task4/decryption_words_alpha.py
@@ -26,20 +26,22 @@ if __name__ == '__main__':
             eng_dict = load(f)
 
         with open(argv[1]) as encrypted:
-            encrypted_str = ''
-            no_spec_encrypted = ''
+            original_str = ''
+            no_spec_str = ''
             for line in encrypted:
-                encrypted_str += ''.join(line)
-                no_spec_encrypted += ''.join(char for char in line if char.isalpha() or char == ' ')
+                original_str += ''.join(line)
+                no_spec_str += ''.join(char for char in line if char.isalpha() or char == ' ')
 
         for num in range(26):
-            shifted_str_list = shift_string(no_spec_encrypted.lower(), num).split()
+            shifted_str_list = shift_string(no_spec_str.lower(), num).split()
             correct_word = 0
-            for word in shifted_str_list:
-                if word in eng_dict[word[0]] and num > 0:
+            for word in shifted_str_list[:]:
+                if len(word) <= 1:
+                    shifted_str_list.remove(word)
+                elif word in eng_dict[word[:2]] and num > 0:
                     correct_word += 1
             if correct_word / len(shifted_str_list) >= 0.70:
-                print(f'\n{shift_string(encrypted_str, num)}')
+                print(f'\n{shift_string(original_str, num)}')
                 break
             elif num == 25 and correct_word / len(shifted_str_list) < 0.70:
                 print('Cannot decrypt. Most likely not a caesar cipher at work here.')
