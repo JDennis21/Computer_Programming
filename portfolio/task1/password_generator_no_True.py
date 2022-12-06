ea55832a3702ac4b1dba05d5c97e41f0964c8886
JDennis21 <114511121+JDennis21@users.noreply.github.com>
2022-12-06 21:20:55 +0000

    No while true loop to satisfy spec

---
+++ portfolio/task1/password_generator_no_True.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+from random import choice
+
+LIST_OF_WORDS = ['dog', 'ham', 'cat', 'knight', 'mighty', 'beast', 'bat', 'cheese', 'knuckles', 'mouse', 'black',
+                 'white', 'red', 'purple', 'green', 'big', 'small', 'massive', 'tiny', 'round', 'square', 'minus',
+                 'plus', 'course', 'phone', 'home', 'feeling', 'happy', 'sad', 'safe', 'careless', 'unhappy']
+
+if __name__ == '__main__':
+
+    print('Password Generator\n==================\n')
+    num_students = input('How many passwords are needed?: ')
+
+    try:
+        if 1 > int(num_students) or int(num_students) > 24:
+            print('Please enter a number between 1 and 24.')
+        else:
+            print('')
+            for count in range(int(num_students)):
+                print(f'{count + 1:>3} --> {"".join(choice(LIST_OF_WORDS) for num in range(3))}')
+    except ValueError:
+        print('Please enter a value between 1 and 24.\n')
