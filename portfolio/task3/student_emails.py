9c049845e27a4f6a3553eae9f0268c2d0152fe15
JDennis21 <114511121+JDennis21@users.noreply.github.com>
2022-12-12 19:54:09 +0000

    same output in less lines using list comprehensions

--- portfolio/task3/student_emails.py
+++ portfolio/task3/student_emails.py
@@ -1,36 +1,24 @@
 #!/usr/bin/env python3
 
+from string import digits
 from random import choice
 from sys import argv
 
 if __name__ == '__main__':
-
-    NUMS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-
     try:
-        argument = argv[1]
-        with open(argument) as student_data:
-            contents = []
-            for line in student_data:
-                contents.append(line.strip())
-            student_data.close()
+        with open(argv[1]) as student_data:
+            contents = [line.strip() for line in student_data]
 
         for data in contents:
             student_id = data[0:8]
             surname = data[9::].split(',', 1)[0]
             names = data[8::].split(',', 1)[1]
-            initials = ''
-            rand_nums = str(choice(NUMS)) + str(choice(NUMS)) + str(choice(NUMS)) + str(choice(NUMS))
-            for character in names:
-                if character.isupper():
-                    initials += f'{character}.'
-            try:
-                output = open('student_data_output.txt', 'x')
+            initials = ''.join((f'{character}.' for character in data[8::].split(',')[1] if character.isupper()))
+            rand_nums = ''.join(choice(digits) for length in range(4))
+
+            with open('student_data_output.txt', 'a') as output:
                 output.write(f'{student_id} {initials.lower()}{surname.lower()}{rand_nums}@poppleton.ac.uk\n')
-            except FileExistsError:
-                output = open('student_data_output.txt', 'a')
-                output.write(f'{student_id} {initials.lower()}{surname.lower()}{rand_nums}@poppleton.ac.uk\n')
-            output.close()
+
     except FileNotFoundError:
         print('Error: File does not exist.')
     except IndexError:
